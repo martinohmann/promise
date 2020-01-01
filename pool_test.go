ca70d0b2a9d5016b0cd79c9ff54d6b2cc77d4057
Martin Ohmann <martin@mohmann.de>
2020-01-01 18:59:37 +0100

    add pool options
    This adds PoolOptions to configure that behaviour of the pool. It also
    adds a panic if a nil event listener is passed.

--- pool_test.go
+++ pool_test.go
@@ -131,6 +131,57 @@ func TestPool_ContextCancel(t *testing.T) {
 	}
 }
 
+func TestPool_ContinueOnError(t *testing.T) {
+	fns := make(chan func() Promise)
+
+	var fulfilled int64
+	var rejected int64
+
+	pool := NewPool(10, fns, PoolOptions{ContinueOnError: true})
+
+	pool.AddEventListener(&PoolEventListener{
+		OnFulfilled: func(val Value) {
+			atomic.AddInt64(&fulfilled, 1)
+		},
+		OnRejected: func(err error) {
+			atomic.AddInt64(&rejected, 1)
+		},
+	})
+
+	go func() {
+		defer close(fns)
+
+		for i := 0; i < 10; i++ {
+			fns <- func(val int) func() Promise {
+				return func() Promise {
+					if val%2 == 0 {
+						return Resolve(nil)
+					}
+
+					return Reject(fmt.Errorf("error in %d", val))
+				}
+			}(i)
+		}
+	}()
+
+	p := pool.Run(context.Background())
+
+	_, err := awaitWithTimeout(t, p, 2*time.Second)
+	if err != nil {
+		t.Fatalf("expected nil error but got: %v", err)
+	}
+
+	n := atomic.LoadInt64(&fulfilled)
+	if n != 5 {
+		t.Fatalf("expected 5 promises to be fulfilled but got %d", n)
+	}
+
+	n = atomic.LoadInt64(&rejected)
+	if n != 5 {
+		t.Fatalf("expected 5 promises to be rejected but got %d", n)
+	}
+}
+
 func TestPool_AddEventListener(t *testing.T) {
 	fns := make(chan func() Promise)
 
@@ -184,6 +235,16 @@ func TestPool_AddEventListener(t *testing.T) {
 	if n < 1 {
 		t.Fatalf("expected 1 or more promises to be rejected but got %d", n)
 	}
+}
+
+func TestPool_AddEventListener_nilListener(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("expected panic")
+		}
+	}()
+
+	NewPool(1, make(chan func() Promise)).AddEventListener(nil)
 }
 
 func TestPool_RemoveEventListener(t *testing.T) {
